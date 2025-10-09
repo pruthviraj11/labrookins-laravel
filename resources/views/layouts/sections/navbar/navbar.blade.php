ad6cff01377390e048e1029e96e966cfbe330efc
Yash <yrabadia99@gmail.com>
2025-10-09 16:26:51 +0530

    changes

--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -432,8 +432,9 @@
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                 <div class="avatar avatar-online">
-                    <img src="{{ Auth::user() ? Auth::user()->profile_photo_url : asset('assets/img/avatars/1.png') }}"
-                        alt class="h-auto rounded-circle">
+<img src="{{asset('assets/img/avatars/16.jpg')}}" alt="">
+                    {{-- <img src="{{ Auth::user() ? Auth::user()->profile_photo_url : asset('assets/img/avatars/1.png') }}"
+                        alt class="h-auto rounded-circle"> --}}
                 </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
